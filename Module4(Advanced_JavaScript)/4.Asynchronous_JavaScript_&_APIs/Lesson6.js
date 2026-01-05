4e67a4b21de0b12134c4595953bfaf2cc70872d1
Pushkar Kumar <pushkarkumar0997@gmail.com>
2026-01-05 21:10:50 +0530

    More Progress

---
+++ Module4(Advanced_JavaScript)/4.Asynchronous_JavaScript_&_APIs/Lesson6.js
@@ -0,0 +1,12 @@
+// https://apis.scrimba.com/dog.ceo/api/breeds/image/random
+
+fetch('https://apis.scrimba.com/dog.ceo/api/breeds/image/random')
+    .then(response => {response.json()})
+    .then(data =>
+    {
+        const imageElement = document.createElement('img')
+        imageElement.src = data.message
+        imageElement.alt = "A Random Dog"
+        document.getElementById('dog-image-container').appendChild(imageElement)
+    }
+    )
---
+++ Module4(Advanced_JavaScript)/4.Asynchronous_JavaScript_&_APIs/url.md
@@ -0,0 +1,9 @@
+/* Base URL */
+https://apis.scrimba.com/dog.ceo/api
+
+/* Endpoints */
+/breeds/list/all
+/breeds/image/random
+
+Challenge:
+    1. Take the base url and the endpoints above and paste it in browser to see the data returned from the API.
