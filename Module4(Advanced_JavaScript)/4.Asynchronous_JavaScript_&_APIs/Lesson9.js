e5608ba37634987fc67a586f38ea458ee852e04e
Pushkar Kumar <pushkarkumar0997@gmail.com>
2026-01-18 22:45:13 +0530

    Some more progress

--- Module4(Advanced_JavaScript)/4.Asynchronous_JavaScript_&_APIs/Lesson9.js
+++ Module4(Advanced_JavaScript)/4.Asynchronous_JavaScript_&_APIs/Lesson9.js
@@ -39,4 +39,5 @@ fetch ('https://apis.scrimba.com/dog.ceo/api/breeds/image/random' )
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(err => console.log("Something went wrong: " + err.message))
-    .finally
+    .finally(() => console.log("Fetch attempt finished."))
+
