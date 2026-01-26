91f986dfd2893d0f04e939461fe60b265a21daf3
Pushkar Kumar <pushkarkumar0997@gmail.com>
2026-01-26 23:45:50 +0530

    tiny bit progress

--- Module4(Advanced_JavaScript)/4.Asynchronous_JavaScript_&_APIs/Lesson9.js
+++ Module4(Advanced_JavaScript)/4.Asynchronous_JavaScript_&_APIs/Lesson9.js
@@ -35,20 +35,62 @@ fetch ('https://apis.scrimba.com/dog.ceo/api/breeds/image/randoms' )
     .catch(err => console.log("Something went wrong: " + err.message))
 */
 
-fetch ('https://apis.scrimba.com/dog.ceo/api/breeds/image/random' )
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.log("Something went wrong: " + err.message))
-    .finally(() => console.log("Fetch attempt finished."))
+// fetch ('https://apis.scrimba.com/dog.ceo/api/breeds/image/random' )
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(err => console.log("Something went wrong: " + err.message))
+//     .finally(() => console.log("Fetch attempt finished."))
 
-// We can also use try and catch block with async-await to handle such errors.
+// We can also use try and catch block with async-await to handle such errors with async await.
 
 try {
-    // the code that we want to run
+    fetch ('https://apis.scrimba.com/dog.ceo/api/breeds/image/random' )
+    const data = await response.json()
+    console.log(data)
 }
 catch (err) {
-    // the code to run if there is an error
+    console.log("Something went wrong: " + err.message)
 }
 finally {
-    // the code that will run regardless of the try/catch result
+    console.log("Fetch attempt finished.")
 }
+
+function print(msg){
+    console.log(msg);
+}
+
+// The finally block will run regardless of whether the promise is fulfilled or rejected.
+// The above code needs to be inside an async function to use await.
+// Otherwise it will throw a syntax error. Cause we need to be inside module 
+// or async function to use await.
+// So we can wrap it inside an async function like below:
+
+async function fetchDogImage() {
+    try {
+        const response = await fetch ('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' )
+        const data = await response.json()
+        console.log(data)
+    }
+    catch (err) {
+        console.log("Something went wrong: " + err.message)
+    }
+    finally {
+        console.log("Fetch attempt finished.")
+    }
+}
+
+fetchDogImage();
+
+// Suppose we break the base url like above.
+// We will get the error message in the catch block.
+// Something went wrong: Failed to fetch
+// Fetch attempt finished.
+// This is how we can handle rejected promises using .catch() method
+// or try-catch block with async-await.
+// But what if we break the endpoint instead of the base url?
+// we will see that the catch block is not executed.
+// This is because the fetch promise is fulfilled as the server is reachable.
+// But the response status is 404 Not Found.
+// So to handle such cases we need to check the response.ok property
+// before parsing the response data.
+// If response.ok is false then we can throw an error to be caught in the catch block.
