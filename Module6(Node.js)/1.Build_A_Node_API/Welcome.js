290d8da4d5166dfab16a3bd2cbd82a66baa6c2af
Pushkar Kumar <pushkarkumar0997@gmail.com>
2026-02-05 23:20:19 +0530

    starting new module

---
+++ Module6(Node.js)/1.Build_A_Node_API/Welcome.js
@@ -0,0 +1,10 @@
+// Welcome to Node
+
+/*
+Back in the day , Web Development was a fragmented process. Front end code run in browser
+with Javascript while backend logic was written in languages like PHP, Python, Ruby, Java,
+C#, etc. This meant that developers had to learn multiple languages and tools to build a 
+complete web application.
+In 2009, Ryan Dahl created Node.js, a runtime environment that allows developers to run JavaScript on the server side. This was a game-changer for web development, as it enabled developers to use a single language (JavaScript) for both front-end and back-end development. Node.js is built on Chrome's V8 JavaScript engine, which makes it fast and efficient for handling server-side tasks.
+Node.js uses an event-driven, non-blocking I/O model, which makes it ideal for building scalable and high-performance
+*/
