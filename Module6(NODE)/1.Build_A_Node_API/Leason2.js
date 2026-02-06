5e4fdb285c3556eeb33e02c6bde89eb71ee4710a
Pushkar Kumar <pushkarkumar0997@gmail.com>
2026-02-06 23:59:30 +0530

    eod progress

--- Module6(NODE)/1.Build_A_Node_API/Leason2.js
+++
@@ -1,7 +0,0 @@
-// The Package.json file
-
-/*
-The package.json file is a crucial component of any Node.js project. It serves as the manifest for the project, providing essential information about the project and its dependencies. The package.json file is typically located at the root of the project directory.
-*/
-
-console.log("Hello, world!")
---
+++ Module6(NODE)/1.Build_A_Node_API/Lesson2.js
@@ -0,0 +1,19 @@
+// The Package.json file
+
+/*
+The package.json file is a crucial component of any Node.js project. It serves as the manifest for the project, providing essential information about the project and its dependencies. The package.json file is typically located at the root of the project directory.
+*/
+
+console.log("Hello, world!")
+
+/*
+
+Package.json is the blueprint!
+- Contains metadata about the project (name, version, description, author, etc.)
+- Simplifies collaborations
+    - Allows developers to easily understand the project and its dependencies
+    - Defining start scripts and other commands that can be run using npm
+- Manages dependencies
+    - Lists all the packages that the project depends on, along with their versions
+    - Allows developers to easily install and manage these dependencies using npm
+*/
