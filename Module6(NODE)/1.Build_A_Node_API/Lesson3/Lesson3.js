0bba38e7048a9d16a1eb381ab6d7a4626ab254a5
Pushkar Kumar <pushkarkumar0997@gmail.com>
2026-02-07 23:24:21 +0530

    eod progress

--- Module6(NODE)/1.Build_A_Node_API/Lesson3/Lesson3.js
+++ Module6(NODE)/1.Build_A_Node_API/Lesson3/Lesson3.js
@@ -1,2 +1,17 @@
 // The HTTP Module
 
+/*
+To create a server in Node.js , we need to include the built-in http module, and then create a server object:
+
+To import the http module , we need to set the type to module in the package.json file, and then we can use the import statement to include the http module in our code. The http module provides a set of functions and classes that allow us to create and manage HTTP servers and clients.
+*/
+
+import http from 'http';
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server!')
+})
+
+server.listen(PORT, () => console.log(`server running on port: ${PORT}`)) 
---
+++ Module6(NODE)/1.Build_A_Node_API/Lesson5/Lesson5.js
@@ -0,0 +1,16 @@
+// Recap
+
+import http from 'node:http'
+
+const PORT = 8000
+
+
+const server = http.createServer( (req, res)=> {
+  res.write('This is some data \n')
+  res.write('This is some more data \n')
+  res.end('This is from the server', 'utf8', ()=> console.log('response end'))
+})
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
+
+// Theory written in the notebook will include them here later.
